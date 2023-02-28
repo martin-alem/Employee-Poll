1b8b65566d437deb3b5e381adeae795c15fe9eea
Martin Alemajoh <alemajohmartin@gmail.com>
2023-02-28 17:01:51 -0500

    project setup

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+export default {
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  testPathIgnorePatterns: ["/node_modules/", "/public/"],
+};
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-3xl text-red-500">
+      <h1>Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
